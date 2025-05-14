488ec6b1c9919e547ae694602632a7173204a4e1
Nicolle Juliana Caldas Pardo <caldaspardo06@gmail.com>
2025-05-14 14:59:51 -0500

    v04

--- docs/programa.js
+++ docs/programa.js
@@ -16,3 +16,17 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
+
+// Abrir archivo GeoJSON
+// JavaScript se ejecuta de tal manera que si una línea de código se demora, se pasa a la siguiente sin esperar.
+// Esto puede generar problemas cuando se requiere secuencialidad .
+
+async function cargarpuntos (){
+    var miArchivo= await fetch("microondas.geojson");
+
+    //Coinvertir el contenido a json: objeto js
+    var datos= await miArchivo.json();
+    console.log(datos);
+};
+
+cargarpuntos();
